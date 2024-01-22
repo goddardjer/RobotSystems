08228677048d5e6a720d595220d4eae27f3d9000
goddardjer <goddarje@oregonstate.edu>
2024-01-22 15:48:30 -0800

    added a loop

--- picarx/week2.py
+++ picarx/week2.py
@@ -2,38 +2,40 @@ import picarx_improved
 
 def main():
     car = picarx_improved.Picarx()
-    user_input = input("Please enter a command (F,B,PL,PR,K): ")
-    
-    if user_input == 'F':
-        # do something for 'F' command
-        speed = int(input("Enter speed: "))
-        time = int(input("Enter time: "))
-        angle = int(input("Enter angle: "))
-        car.Manuevering_fwd_at_angle(speed, time, angle)
-        print("Forward command executed.")
-    
-    elif user_input == 'B':
-        # do something for 'B' command
-        speed = int(input("Enter speed: "))
-        time = int(input("Enter time: "))
-        angle = int(input("Enter angle: "))
-        car.Manuevering_back_at_angle(speed, time, angle)
-        print("Backward command executed.")
-    
-    elif user_input == 'PL':
-        # do something for 'PL' command
-        print("Parallel left command executed.")
-    
-    elif user_input == 'PR':
-        # do something for 'PR' command
-        print("Parallel right command executed.")
-    
-    elif user_input == 'K':
-        # do something for 'K' command
-        print("K turn command executed.")
-    
-    else:
-        print("Invalid command. Please enter a valid command.")
+
+    while True:
+        user_input = input("Please enter a command (F,B,PL,PR,K): ")
+
+        if user_input == 'F':
+            # do something for 'F' command
+            speed = int(input("Enter speed: "))
+            time = int(input("Enter time: "))
+            angle = int(input("Enter angle: "))
+            car.Manuevering_fwd_at_angle(speed, time, angle)
+            print("Forward command executed.")
+
+        elif user_input == 'B':
+            # do something for 'B' command
+            speed = int(input("Enter speed: "))
+            time = int(input("Enter time: "))
+            angle = int(input("Enter angle: "))
+            car.Manuevering_back_at_angle(speed, time, angle)
+            print("Backward command executed.")
+
+        elif user_input == 'PL':
+            # do something for 'PL' command
+            print("Parallel left command executed.")
+
+        elif user_input == 'PR':
+            # do something for 'PR' command
+            print("Parallel right command executed.")
+
+        elif user_input == 'K':
+            # do something for 'K' command
+            print("K turn command executed.")
+
+        else:
+            print("Invalid command. Please enter a valid command.")
 
 
 if __name__ == "__main__":
