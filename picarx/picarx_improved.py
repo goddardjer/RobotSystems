bef1af0f13733a166e132864fce44177a490703e
goddardjer <goddarje@oregonstate.edu>
2024-01-22 14:35:39 -0800

    ackerman for fwd

--- picarx/picarx_improved.py
+++ picarx/picarx_improved.py
@@ -243,24 +243,34 @@ class Picarx(object):
 
     #Chat GPT helped Function
     def forward(self, speed):
+        # current_angle = self.dir_current_angle
+        # if current_angle != 0:
+        #     abs_current_angle = abs(current_angle)
+        #     if abs_current_angle > self.DIR_MAX:
+        #         abs_current_angle = self.DIR_MAX
+        #     power_scale = (100 - abs_current_angle) / 100.0
+        #     if (current_angle / abs_current_angle) > 0:
+        #         self.set_motor_speed(1, 1*speed * power_scale)
+        #         self.set_motor_speed(2, -speed) 
+        #     else:
+        #         self.set_motor_speed(1, speed)
+        #         self.set_motor_speed(2, -1*speed * power_scale)
+        # else:
+        #     self.set_motor_speed(1, speed)
+        #     self.set_motor_speed(2, -1*speed) 
         current_angle = self.dir_current_angle
         if current_angle != 0:
             abs_current_angle = abs(current_angle)
             if abs_current_angle > self.DIR_MAX:
                 abs_current_angle = self.DIR_MAX
             power_scale = (100 - abs_current_angle) / 100.0
-            if (current_angle / abs_current_angle) > 0:
-                self.set_motor_speed(1, 1*speed * power_scale)
-                self.set_motor_speed(2, -speed) 
-            else:
-                self.set_motor_speed(1, speed)
-                self.set_motor_speed(2, -1*speed * power_scale)
+            left_speed, right_speed = self.ackermann_steering(current_angle)
+            self.set_motor_speed(1, speed * left_speed * power_scale)
+            self.set_motor_speed(2, -1 * speed * right_speed * power_scale)
         else:
             self.set_motor_speed(1, speed)
-            self.set_motor_speed(2, -1*speed) 
-        # left_speed, right_speed = self.ackermann_steering(current_angle)
-        # self.set_motor_speed(1, speed * left_speed)
-        # self.set_motor_speed(2, speed * right_speed)                
+            self.set_motor_speed(2, -1 * speed)
+                        
 
     def stop(self):
         '''
