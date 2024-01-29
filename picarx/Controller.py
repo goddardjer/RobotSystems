2f689e4d8d593711b7d016090533202171930d1e
goddardjer <goddarje@oregonstate.edu>
2024-01-29 12:50:36 -0800

    kk

--- picarx/Controller.py
+++ picarx/Controller.py
@@ -2,7 +2,9 @@ try:
     from robot_hat import ADC
 except ImportError:
     from sim_robot_hat import ADC
+import picarx_improved as pixi
 import Interpreter
+import Sensor
 import time
 
 
@@ -12,15 +14,21 @@ class Controller(object):
         self.interpreter = interpreter
         self.scaling_factor = scaling_factor
 
-    def control(self, car):
+    def control(self):
         # Get the offset from the interpreter
         offset = self.interpreter.interpret()
 
         # Calculate the steering angle based on the offset and scaling factor
         steering_angle = offset * self.scaling_factor
 
-        # Steer the car
-        car.steer(steering_angle)
-
         # Return the commanded steering angle
         return steering_angle
+    
+if __name__ == '__main__':
+    car = pixi.Picarx()
+    sensor = Sensor.Sensor()
+    interpreter = Interpreter.Interpreter(sensor)
+    controller = Controller(interpreter)
+    while True:
+        car.Manuevering_fwd_at_angle(40,1,controller.control())
+        time.sleep(1)
